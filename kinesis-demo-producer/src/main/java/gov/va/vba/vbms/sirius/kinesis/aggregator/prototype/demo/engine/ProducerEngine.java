a015bfc6752c3816ab44ca9350c1cdd6e01afa73
bah-atimes <582309@bah.com>
2020-07-02 15:17:07 -0400

    Experiment to move producer work to a thread

--- kinesis-demo-producer/src/main/java/gov/va/vba/vbms/sirius/kinesis/aggregator/prototype/demo/engine/ProducerEngine.java
+++ kinesis-demo-producer/src/main/java/gov/va/vba/vbms/sirius/kinesis/aggregator/prototype/demo/engine/ProducerEngine.java
@@ -5,12 +5,14 @@ import com.amazonaws.kinesis.agg.RecordAggregator;
 import com.amazonaws.services.kinesis.AmazonKinesis;
 import gov.va.vba.vbms.sirius.kinesis.aggregator.prototype.demo.config.ProducerConfig;
 import gov.va.vba.vbms.sirius.kinesis.aggregator.prototype.demo.util.ProducerUtils;
+import javafx.concurrent.Worker;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
 import java.util.Random;
+import java.util.concurrent.ConcurrentLinkedQueue;
 
 @Component
 @Slf4j
@@ -24,6 +26,8 @@ public class ProducerEngine {
 
     AmazonKinesis amazonKinesis;
 
+    ConcurrentLinkedQueue<AggRecord> queue;
+
     ProducerEngine(AmazonKinesis kinesis) {
         this.amazonKinesis = kinesis;
     }
@@ -34,6 +38,11 @@ public class ProducerEngine {
             amazonKinesis.createStream(config.STREAM_NAME, 1);
             log.info("stream " + config.STREAM_NAME + " created.");
         }
+        queue = new ConcurrentLinkedQueue();
+
+        WorkerBee bee = new WorkerBee(queue, amazonKinesis);
+        Thread queenBee = new Thread(bee);
+        queenBee.start();
     }
 
     public void generateAndPublish(RecordAggregator aggregator) {
@@ -41,7 +50,6 @@ public class ProducerEngine {
         long count = 1;
 
         while(true) {
-//            String ehk = util.randomExplicitHashKey();
             for (int i = 1; i <= config.RECORDS_TO_TRANSMIT; i++) {
 
                 long claimId = Math.abs(new Random().nextInt());
@@ -52,11 +60,7 @@ public class ProducerEngine {
                 try {
                     final AggRecord aggRecord = aggregator.addUserRecord(new Long(claimId % 10).toString(), data);
                     if (aggRecord != null) {
-                        sendRecord(aggRecord);
-//                        ehk = util.randomExplicitHashKey();
-//                    ForkJoinPool.commonPool().execute(() -> {
-//                        sendRecord(aggRecord);
-//                    });
+                        queue.add(aggRecord);
                     }
                 } catch (Exception e) {
                     e.printStackTrace();
@@ -73,33 +77,7 @@ public class ProducerEngine {
     }
 
     private void flushAndFinish(RecordAggregator aggregator) {
-        // Do one final flush & send to get any remaining records that haven't
-        // triggered a callback yet
         AggRecord finalRecord = aggregator.clearAndGet();
-        sendRecord(finalRecord);
-//        ForkJoinPool.commonPool().execute(() -> {
-//            sendRecord(finalRecord);
-//        });
-//
-//        // Wait up to 2 minutes for all the publisher threads to finish
-//        log.info("Waiting for all transmissions to complete...");
-//        ForkJoinPool.commonPool().awaitQuiescence(2, TimeUnit.MINUTES);
-//        log.info("Transmissions complete.");
-    }
-
-
-    private void sendRecord(AggRecord aggRecord) {
-        if (aggRecord == null || aggRecord.getNumUserRecords() == 0) {
-            return;
-        }
-
-        log.info("Submitting record EHK=" + aggRecord.getExplicitHashKey() + " NumRecords="
-                + aggRecord.getNumUserRecords() + " NumBytes=" + aggRecord.getSizeBytes());
-        try {
-            amazonKinesis.putRecord(aggRecord.toPutRecordRequest(config.STREAM_NAME));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        log.info("Completed record EHK=" + aggRecord.getExplicitHashKey());
+        queue.add(finalRecord);
     }
 }
---
+++ kinesis-demo-producer/src/main/java/gov/va/vba/vbms/sirius/kinesis/aggregator/prototype/demo/engine/WorkerBee.java
@@ -0,0 +1,59 @@
+package gov.va.vba.vbms.sirius.kinesis.aggregator.prototype.demo.engine;
+
+import com.amazonaws.kinesis.agg.AggRecord;
+import com.amazonaws.services.kinesis.AmazonKinesis;
+import com.amazonaws.services.kinesis.producer.UserRecord;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.stereotype.Component;
+
+import java.util.Iterator;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+@Slf4j
+public class WorkerBee implements Runnable {
+
+    AmazonKinesis amazonKinesis;
+    ConcurrentLinkedQueue<AggRecord> queue;
+
+
+    public WorkerBee(ConcurrentLinkedQueue<AggRecord> queue, AmazonKinesis kinesis) {
+        this.queue = queue;
+        this.amazonKinesis = kinesis;
+    }
+
+    public void run() {
+        while (true) {
+            log.info("Hello from a thread!");
+
+            if(queue.size() > 0) {
+                Iterator<AggRecord> iter = queue.iterator();
+                while(iter.hasNext()) {
+                    AggRecord aggRecord = iter.next();
+                    sendRecord(aggRecord);
+                    iter.remove();
+                }
+            }
+
+            try { Thread.sleep(1000); } catch (InterruptedException ie) {}
+        }
+    }
+
+    private void sendRecord(AggRecord aggRecord) {
+        if (aggRecord == null || aggRecord.getNumUserRecords() == 0) {
+            return;
+        }
+
+        log.info("Submitting record EHK=" + aggRecord.getExplicitHashKey() + " NumRecords="
+                + aggRecord.getNumUserRecords() + " NumBytes=" + aggRecord.getSizeBytes());
+        try {
+            synchronized (amazonKinesis) {
+                amazonKinesis.putRecord(aggRecord.toPutRecordRequest("test_stream"));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        log.info("Completed record EHK=" + aggRecord.getExplicitHashKey());
+    }
+
+}
